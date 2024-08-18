6ac20ca92618abd3d56e199074f5eff0d63708e3
md-mannan <mdmannan580@gmail.com>
2024-08-18 09:12:02 +0300

    Access modefire public

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
 
-class Person
+// PHP Access Modefiers
+
+class Base
 {
-    public $name, $age;
-    function __construct($name, $age = 18)
+    public $name; //this $variable can be access from anywhere object of class or child class
+
+    public function __construct($name)
     {
         $this->name = $name;
-        $this->age = $age;
     }
-    function show()
+
+    function display()
     {
-        return "User Name is: " . $this->name . " and Age is: " . $this->age . "</br>";
+        echo "Hello, My Name is " . $this->name . "<br>";
     }
 }
 
-$object = new Person('Mannan', 25);
-echo $object->show();
-
-$object2 = new Person('Ravi');
-echo $object2->show();
+$obj = new Base("Mannan");
+$obj->display();

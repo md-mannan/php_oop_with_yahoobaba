143352d69813a0ea761219fa59af39f69d25cf86
md-mannan <mdmannan580@gmail.com>
2024-08-18 13:20:20 +0300

    use of static keyword

--- index.php
+++ index.php
@@ -3,24 +3,24 @@
 
 interface parent1
 {
-    function calc($a, $b);
+    static function calc($a, $b);
 }
 interface parent2
 {
-    function sub($c, $d);
+    static function sub($c, $d);
 }
 class childClass implements parent1, parent2
 {
-    public function calc($a, $b)
+    public static function calc($a, $b)
     {
         echo "Addition of two numbers: " . ($a + $b) . "</br>";
     }
-    public function sub($c, $d)
+    public static function sub($c, $d)
     {
         echo "Subtraction of two numbers: " . ($c - $d) . "</br>";
     }
 }
 
-$obj = new childClass();
-$obj->calc(5, 80);
-$obj->sub(50, 10);
+// $obj = new childClass();
+childClass::calc(5, 80);
+childClass::sub(50, 10);

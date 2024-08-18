bfe88304ec1c7feceab06430fca7126b6f2740be
md-mannan <mdmannan580@gmail.com>
2024-08-18 21:35:17 +0300

    method chaining

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 
 
-require 'first.php';
-require 'second.php';
+class abc
+{
+    function first()
+    {
+        echo "Hello from first function\n";
+        return $this;
+    }
+    function second()
+    {
+        echo "Hello from second function\n";
+        return $this;
+    }
+    function third()
+    {
+        echo "Hello from third function\n";
+        return $this;
+    }
+}
 
-$first = new first\first();
-$first->sayHello();
-$second = new second\second();
-$second->sayHi();
+$test = new abc();
+$test->first()->second()->third();
